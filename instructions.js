a98f57b5cf652525145323da89c8b4006c670246
hankerson <hanksingh07@gmail.com>
2021-12-10 09:09:17 -0500

    0.0.0

---
+++ instructions.js
@@ -0,0 +1,15 @@
+// {npm init -y}; for package.json
+
+// dependencies: 
+
+// {express}; for server
+// {socket.io}; for socket.io
+// {ejs}; for templating
+// {uuid}; for unique id
+// dev dependencies: {nodemon,}
+
+
+// npm i express socket.io ejs
+// npm i uuid
+// npm i --save-dev nodemon
+
